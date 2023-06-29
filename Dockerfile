364d7c324cd4637b7fffdd7b5d2398ac7e0fbdbd
Jonas Rosland <jonas.rosland@gmail.com>
2023-06-29 16:56:38 -0400

    Use environment variables for Docker instead of a configuration file
    Signed-off-by: Jonas Rosland <jonas.rosland@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,4 @@
 FROM python:3.10.9-slim-bullseye
-RUN apt-get update && apt-get install -y --no-install-recommends \
-    git
-RUN git clone https://github.com/tubearchivist/jellyfin
+COPY . /jellyfin
 WORKDIR jellyfin
 RUN pip install -r requirements.txt
--- src/config.py
+++ src/config.py
@@ -1,13 +1,28 @@
 """handle config file"""
 
 import json
+import os
 
 from src.static_types import ConfigType
 
 
 def get_config() -> ConfigType:
     """get connection config"""
-    with open("config.json", "r", encoding="utf-8") as f:
-        config_content: ConfigType = json.loads(f.read())
-
-    return config_content
+    
+    if os.path.exists("config.json"):
+        print("config.json file found, skipping environment variables")
+        with open("config.json", "r", encoding="utf-8") as f:
+            config_content: ConfigType = json.loads(f.read())
+        return config_content
+    elif "TA_URL" in os.environ:
+        print("Environment variables found, continuing")
+        data = {}
+        data['ta_video_path'] = os.getenv('TA_VIDEO_PATH', '/youtube')
+        data['ta_url'] = os.getenv('TA_URL')
+        data['ta_token'] = os.getenv('TA_TOKEN')
+        data['jf_url'] = os.getenv('JF_URL')
+        data['jf_token'] = os.getenv('JF_TOKEN')
+        config_content: ConfigType = json.loads(json.dumps(data))
+        return config_content
+    else:
+        raise ValueError("No config.json or environment variable found, exiting")
